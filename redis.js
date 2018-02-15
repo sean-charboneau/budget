2e8155ce37b22576c8a1252eb11347f969c587a0
Sean Charboneau <seancharboneau@gmail.com>
2018-02-15 22:04:16 +0100

    redis env for heroku, take two

--- redis.js
+++ redis.js
@@ -1,7 +1,11 @@
 var config = require('config');
 var redis = require('redis');
 
-var client = redis.createClient(config.get('redis.port'), process.env.REDIS_URL || config.get('redis.hostname'));
+var client = redis.createClient(
+    process.env.REDIS_PORT || config.get('redis.port'),
+    process.env.REDIS_HOST || config.get('redis.hostname')
+);
+
 client.on('connect', function() {
     console.log('Redis client connected at ' + config.get('redis.hostname') + ':' + config.get('redis.port'));
 });
